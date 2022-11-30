95b75f5481a2a0442985a512868a2d1f0eebba37
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-11-30 18:37:10 +0600

    Scraping data with category ids

--- parser.py
+++ parser.py
@@ -1,4 +1,9 @@
-def get_category_id():
+import json
+
+import requests
+
+
+def get_category_ids():
     ids = []
     with open('links') as links_file:
         lines = links_file.readlines()
@@ -10,4 +15,31 @@ def get_category_id():
     return ids
 
 
-ids = get_category_id()
+category_ids = get_category_ids()
+
+url = 'https://lalafo.kg/api/search/v3/feed/search'
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                  (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+    "Accept": "application/json, text/plain, */*",
+    "device": "pc"
+}
+
+
+def get_json():
+    categories = []
+    for category_id in category_ids:
+        params = {
+            "expand": "url",
+            'per-page': 16,
+            'category_id': category_id,
+        }
+        response = requests.get(url, headers=headers, params=params)
+        categories.append({
+            category_id: [response.json()]
+        })
+    return categories
+
+
+data_json = get_json()
