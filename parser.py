eb60084cb6bb6eefb444af8c35f9a581526da973
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-12-01 18:42:47 +0600

    Filter scraped data

--- parser.py
+++ parser.py
@@ -15,31 +15,73 @@ def get_category_ids():
     return ids
 
 
-category_ids = get_category_ids()
-
-url = 'https://lalafo.kg/api/search/v3/feed/search'
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-                  (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-    "Accept": "application/json, text/plain, */*",
-    "device": "pc"
-}
-
-
 def get_json():
-    categories = []
+    categories = {}
     for category_id in category_ids:
         params = {
             "expand": "url",
-            'per-page': 16,
+            'per-page': 20,
             'category_id': category_id,
         }
         response = requests.get(url, headers=headers, params=params)
-        categories.append({
-            category_id: [response.json()]
-        })
+        categories[category_id] = response.json()
     return categories
 
 
-data_json = get_json()
+def get_data_from_json(json_file):
+    thumbnail_link = 'https://img5.lalafo.com/i/posters/api'
+    filtered_data = {}
+    filtered_items = []
+    for category_id, value in json_file.items():
+        for index, item in enumerate(value['items']):
+            if index == 20:
+                break
+            title = item['title']
+            description = item['description'].replace('\n', ' ')
+            price = item['price']
+            city = item['city']
+            try:
+                thumbnail = thumbnail_link + item['image']
+            except TypeError:
+                thumbnail = 'Без изображения'
+            images = []
+            for i in item['images']:
+                image = i['original_url']
+                images.append(image)
+            phone = item['mobile']
+            try:
+                author = item['user']['username']
+            except:
+                author = ''
+            filtered_items.append({
+                'title': title,
+                'description': description,
+                'price': price,
+                'city': city,
+                'thumbnail': thumbnail,
+                'images': images,
+                'phone': phone,
+                'author': author,
+            })
+        filtered_data[category_id] = filtered_items
+        filtered_items = []
+    return filtered_data
+
+
+def create_filtered_json(data):
+    with open('lalafo_data.json', 'w', encoding='UTF-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    category_ids = get_category_ids()
+    url = 'https://lalafo.kg/api/search/v3/feed/search'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                      (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "device": "pc"
+    }
+    data_json = get_json()
+    filtered_json = get_data_from_json(data_json)
+    create_filtered_json(filtered_json)
