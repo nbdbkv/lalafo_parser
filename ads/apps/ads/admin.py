1010d58bbdf9380e9ce2e12c60e523d2c8595d61
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-12-05 18:49:42 +0600

    Add data to admin

--- ads/apps/ads/admin.py
+++ ads/apps/ads/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Ad, Image
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    # ordering = ('id',)
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'price', 'city', 'category', 'thumbnail_link', 'phone', 'author')
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image_link')
