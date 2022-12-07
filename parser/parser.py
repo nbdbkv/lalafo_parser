05f987c7c86d3ffcf8b56224c636ba0d8d1b079b
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-12-07 12:25:04 +0600

    Update parser

--- parser/parser.py
+++ parser/parser.py
@@ -3,36 +3,37 @@ import json
 import requests
 
 
-def get_category_ids():
-    ids = []
+def get_category_data():
+    categories = {}
     with open('links') as links_file:
         lines = links_file.readlines()
-        for link_id in lines:
-            if link_id.__contains__("category_id"):
-                start = link_id.find("category_id=") + len("category_id=")
-                end = link_id.find(" ")
-                ids.append(link_id[start:end])
-    return ids
+        for line in lines:
+            if line.__contains__("category_id") and line.__contains__("a."):
+                start_id = line.find("category_id=") + len("category_id=")
+                end_id = line.find(" ")
+                start_name = line.find("a. ") + len("a. ")
+                end_name = line.find(" .a")
+                categories[line[start_name:end_name]] = line[start_id:end_id]
+    return categories
 
 
-def get_json():
-    categories = {}
-    for category_id in category_ids:
+def get_json_data(data):
+    for category_name, category_id in data.items():
         params = {
             "expand": "url",
             'per-page': 20,
             'category_id': category_id,
         }
         response = requests.get(url, headers=headers, params=params)
-        categories[category_id] = response.json()
-    return categories
+        data[category_name] = response.json()
+    return data
 
 
-def get_data_from_json(json_file):
+def filter_json_data(json_file):
     thumbnail_link = 'https://img5.lalafo.com/i/posters/api'
     filtered_data = {}
     filtered_items = []
-    for category_id, value in json_file.items():
+    for key, value in json_file.items():
         for index, item in enumerate(value['items']):
             if index == 20:
                 break
@@ -63,18 +64,18 @@ def get_data_from_json(json_file):
                 'phone': phone,
                 'author': author,
             })
-        filtered_data[category_id] = filtered_items
+        filtered_data[key] = filtered_items
         filtered_items = []
     return filtered_data
 
 
-def create_filtered_json(data):
+def create_filtered_json_file(data):
     with open('lalafo_data.json', 'w', encoding='UTF-8') as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    category_ids = get_category_ids()
+    category_data = get_category_data()
     url = 'https://lalafo.kg/api/search/v3/feed/search'
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
@@ -82,6 +83,6 @@ if __name__ == '__main__':
         "Accept": "application/json, text/plain, */*",
         "device": "pc"
     }
-    data_json = get_json()
-    filtered_json = get_data_from_json(data_json)
-    create_filtered_json(filtered_json)
+    json_data = get_json_data(category_data)
+    filtered_json_data = filter_json_data(json_data)
+    create_filtered_json_file(filtered_json_data)
