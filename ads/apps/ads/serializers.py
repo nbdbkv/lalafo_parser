6cd32c7a3f60c9f06b8e376c4e391d808fc3d196
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-12-09 19:00:53 +0600

    Add create method to AdsByCategorySerializer

--- ads/apps/ads/serializers.py
+++ ads/apps/ads/serializers.py
@@ -4,29 +4,39 @@ from .models import Category, Image, Ad
 
 
 class CategoryListSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
         fields = ('id', 'name')
 
 
 class ImageListSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Image
         fields = ('image_link',)
 
 
 class AdsByCategorySerializer(serializers.ModelSerializer):
-    category = serializers.CharField(source="category.name", read_only=True)
     images = ImageListSerializer(many=True)
+
     class Meta:
         model = Ad
         fields = (
             'id', 'title', 'description', 'price', 'city', 'category', 'thumbnail_link', 'images', 'phone', 'author',
         )
+        read_only_fields = ('id',)
 
+    def create(self, validated_data):
+        images = validated_data.pop('images')
+        ad = Ad.objects.create(**validated_data)
+        for image in images:
+            Image.objects.create(ad=ad, **image)
+        return ad
 
 class AdDetailSerializer(serializers.ModelSerializer):
     images = ImageListSerializer(many=True)
+
     class Meta:
         model = Ad
         fields = (
