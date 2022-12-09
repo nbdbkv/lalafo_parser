beb3cf5e856ca8fe950574b073beeb42a4030c1e
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2022-12-09 19:04:33 +0600

    Post filtered JSON data to PostgreSQL

--- parser/parser.py
+++ parser/parser.py
@@ -29,11 +29,11 @@ def get_json_data(data):
     return data
 
 
-def filter_json_data(json_file):
+def filter_json_data(json_data):
     thumbnail_link = 'https://img5.lalafo.com/i/posters/api'
     filtered_data = {}
     filtered_items = []
-    for key, value in json_file.items():
+    for key, value in json_data.items():
         for index, item in enumerate(value['items']):
             if index == 20:
                 break
@@ -69,9 +69,58 @@ def filter_json_data(json_file):
     return filtered_data
 
 
-def create_filtered_json_file(data):
+def create_filtered_json_file(filtered_data):
     with open('lalafo_data.json', 'w', encoding='UTF-8') as file:
-        json.dump(data, file, indent=2, ensure_ascii=False)
+        json.dump(filtered_data, file, indent=2, ensure_ascii=False)
+
+
+def get_category_from_db():
+    response = requests.get("http://localhost:8000/api/categories/")
+    return response.json()
+
+
+def post_category_to_db(new_category):
+    payload = {'name': new_category}
+    response = requests.post("http://localhost:8000/api/categories/", json=payload)
+    return response
+
+
+def check_category_in_db(filtered_data):
+    category_from_db = get_category_from_db()
+    categories = {}
+    for category in [*filtered_data]:
+        if not category_from_db:
+            post_category_to_db(category)
+        for pair in category_from_db:
+            if category in pair['name']:
+                categories[category] = pair['id']
+            else:
+                post_category_to_db(category)
+    return categories
+
+
+def post_json_to_postgres(filtered_data):
+    for _ in range(1):
+        check_category_in_db(filtered_data)
+    categories = check_category_in_db(filtered_data)
+    for key_1, value_1 in filtered_data.items():
+        for name, id in categories.items():
+            if key_1 == name:
+                for item in value_1:
+                    images = [dict(zip(['image_link'], [x])) for x in item['images']]
+                    payload = {
+                        'title': item['title'],
+                        'description': item['description'],
+                        'price': item['price'],
+                        'city': item['city'],
+                        'category': id,
+                        'thumbnail_link': item['thumbnail'],
+                        'images': images,
+                        'phone': item['phone'],
+                        'author': item['author']
+                    }
+                    response = requests.post("http://localhost:8000/api/categories/" + str(id) + "/", json=payload)
+    return response
 
 
 if __name__ == '__main__':
@@ -86,3 +135,4 @@ if __name__ == '__main__':
     json_data = get_json_data(category_data)
     filtered_json_data = filter_json_data(json_data)
     create_filtered_json_file(filtered_json_data)
+    post_json_to_postgres(filtered_json_data)
